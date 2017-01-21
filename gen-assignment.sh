2caf42ba824913433e080cfae55093f8cd54ccf5
Vince Cellini <vincecellini@Vinces-MacBook-Pro.local>
2017-01-21 12:26:49 -0500

    final script

--- gen-assignment.sh
+++ gen-assignment.sh
@@ -4,6 +4,4 @@ echo "creating assignment-$1"
 mkdir $1
 cd $1
 mkdir css js images
-touch index.html README.md style.css app.js
-mv style.css css
-mv app.js js
+touch index.html README.md ./css/style.css ./js/app.js
